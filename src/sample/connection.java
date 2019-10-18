a3dbaeef1635219841750b8997f3e5abbd890a02
juanatm <juanblancoperales@gmail.com>
2019-10-18 15:55:19 +0200

    database

---
+++ src/sample/connection.java
@@ -0,0 +1,30 @@
+package sample;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class connection {
+    public static void main(String[] args) {
+        Connection conn = null;
+        try {
+            String url = "jdbc:sqlite:C:/Users/ juanb/desktop/Trains2.db";
+            conn = DriverManager.getConnection(url);
+
+            System.out.println("Got it!");
+
+        } catch (SQLException e) {
+            throw new Error("Problem", e);
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+}
