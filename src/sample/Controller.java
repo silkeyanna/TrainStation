679d33bcad4688b7cc7e0a5b6758310fb33a21e0
Silke <syjonk@ruc.dk>
2019-10-18 15:40:49 +0200

    JavaFX

---
+++ src/sample/Controller.java
@@ -0,0 +1,70 @@
+package sample;
+
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+
+public class Controller {
+
+    TrainModel m= TrainModel.getInstance(); //make a model object when you create the controller
+    @FXML
+    TextField stat1;
+    @FXML
+    TextField stat2;
+    @FXML
+    TextField time;
+    @FXML
+    TextField res;
+
+    @FXML
+    public void routeHandler(ActionEvent e){
+        System.out.println("find route");
+        res.setText(m.findRoute(stat1.getText(),stat2.getText(),time.getText()));
+    }
+}
+
+class TrainModel{ //shouldnt know anything about the GUI world
+    private TrainModel(){ //private because it should be a singleton!
+    } //no body because it is a constructor
+
+    static TrainModel inst;
+    static TrainModel getInstance(){if (inst==null) inst=new TrainModel(); return inst;} //only one object of trainmodel class will be created this way
+
+
+    String[] getStations() {String[] s = {"Kobenhavn", "Roskilde", "Odense"}; return s;}
+    String findRoute(String stat1, String stat2, String time){return "route from" + stat1+ "\n to " + stat2 + " at "+ time;}
+}
+
+    /*
+    @FXML
+    Button btn;
+
+    @FXML
+    TextField password;
+
+    @FXML
+    TextField txt;
+
+
+    public void initialize() { //executed when GUI is ready
+        btn.addEventHandler(ActionEvent.ACTION, new EventHandler<Event>() { //Ctrl Space Enter
+            @Override
+            public void handle(Event event) {
+                if (password.getText().equals("admin")) { //equals because they are objects and objects can be complex to compare
+                    txt.setVisible(true);
+                }
+
+            }
+        });
+
+    }
+
+}
+
+     */
